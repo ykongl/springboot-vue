7ed48158d9434578bd4b04022546e199ea1ff6e4
ykongl <57851069+ykongl@users.noreply.github.com>
2021-09-18 17:35:54 +0800

    Add files via upload

--- sport.sql
+++ sport.sql
@@ -1,88 +1,115 @@
 /*
-SQLyog Community v13.1.6 (64 bit)
-MySQL - 5.7.19 : Database - hsp_db03
-*********************************************************************
+ Navicat Premium Data Transfer
+
+ Source Server         : 123
+ Source Server Type    : MySQL
+ Source Server Version : 80026
+ Source Host           : localhost:3306
+ Source Schema         : sport
+
+ Target Server Type    : MySQL
+ Target Server Version : 80026
+ File Encoding         : 65001
+
+ Date: 18/09/2021 17:34:58
 */
 
-/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`hsp_db03` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-
-
-/*Table structure for table `easyuser` */
-
+-- ----------------------------
+-- Table structure for easyuser
+-- ----------------------------
 DROP TABLE IF EXISTS `easyuser`;
+CREATE TABLE `easyuser`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用戶名稱',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密碼',
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '郵箱地址',
+  `roleId` int NULL DEFAULT NULL COMMENT '角色',
+  `state` tinyint NULL DEFAULT 0 COMMENT '狀態',
+  `isVip` tinyint(1) NULL DEFAULT 0 COMMENT '是否是會員',
+  `delete_flag` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '刪除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `easyuser` (
-  `id` int(24) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `role` varchar(255) NOT NULL,
-  `state` tinyint(255) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of easyuser
+-- ----------------------------
+INSERT INTO `easyuser` VALUES (1, 'admin', '123456', '123456@qq.com', 1, 1, 0, '0');
+INSERT INTO `easyuser` VALUES (2, 'naughty', '888888', '456@qq.com', 2, 1, 0, '0');
+INSERT INTO `easyuser` VALUES (3, 'flysky01', '987654', 'qwe@qq.com', 3, 1, 1, '0');
+INSERT INTO `easyuser` VALUES (4, 'anglybirds', '321654', 'aaaa@163.com', 3, 0, 1, '0');
+INSERT INTO `easyuser` VALUES (5, 'admin003', 'q123456', 'c@qq.com', 4, 1, 0, '0');
+INSERT INTO `easyuser` VALUES (6, 'admin005', '123456', 'cxz@qq.com', 4, 0, 0, '0');
+INSERT INTO `easyuser` VALUES (7, 'admin007', '123456', 'rty@qq.com', 4, 0, 0, '0');
+INSERT INTO `easyuser` VALUES (8, 'ykongl', '123456', 'Mjy@163.com', 3, 0, 1, '0');
+INSERT INTO `easyuser` VALUES (9, 'xuyin', '123456', 'xuyin@qq.com', 3, 0, 1, '0');
+INSERT INTO `easyuser` VALUES (12, 'wenzi', '123456', 'wenzi@qq.com', 3, 0, 0, '0');
+INSERT INTO `easyuser` VALUES (13, 'asd', '123456', 'asd@163.com', 3, 0, 0, '0');
 
-/*Data for the table `easyuser` */
-
-insert  into `easyuser`(`id`,`username`,`password`,`email`,`role`,`state`) values 
-(1,'admin','123456','123@qq.com','超级管理员',1),
-(2,'naughty','888888','456@qq.com','普通管理员',0),
-(3,'flysky01','987654','qwe@qq.com','普通用户',1),
-(4,'anglybirds','321654','aaaa@163.com','普通用户',0),
-(7,'admin003','q123456','c@qq.com','普通用户',1),
-(8,'admin005','123456','cxz@qq.com','普通用户',0),
-(9,'admin007','123456','rty@qq.com','普通用户',0);
-
-/*Table structure for table `mainmenu` */
-
+-- ----------------------------
+-- Table structure for mainmenu
+-- ----------------------------
 DROP TABLE IF EXISTS `mainmenu`;
+CREATE TABLE `mainmenu`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 201 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `mainmenu` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `path` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=201 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of mainmenu
+-- ----------------------------
+INSERT INTO `mainmenu` VALUES (100, '用戶管理', '/admin');
+INSERT INTO `mainmenu` VALUES (200, '运动平台', '/use');
+INSERT INTO `mainmenu` VALUES (300, '運動商品', '/mall');
+INSERT INTO `mainmenu` VALUES (400, '教練員', '/teacher');
 
-/*Data for the table `mainmenu` */
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '主鍵Id',
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL COMMENT '角色',
+  `roleId` int NULL DEFAULT NULL COMMENT '関連easyuser表',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
 
-insert  into `mainmenu`(`id`,`title`,`path`) values 
-(100,'权限管理','/admin'),
-(200,'运动平台','/use');
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, '超級管理員', 1);
+INSERT INTO `role` VALUES (2, '管理員', 2);
+INSERT INTO `role` VALUES (3, 'VIP會員', 3);
+INSERT INTO `role` VALUES (4, '普通用戶', 4);
 
-/*Table structure for table `submenu` */
-
+-- ----------------------------
+-- Table structure for submenu
+-- ----------------------------
 DROP TABLE IF EXISTS `submenu`;
+CREATE TABLE `submenu`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `path` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `mid` int NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `mid`(`mid`) USING BTREE,
+  CONSTRAINT `submenu_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `mainmenu` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 204 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
-CREATE TABLE `submenu` (
-  `id` int(50) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `path` varchar(255) NOT NULL,
-  `mid` int(50) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `mid` (`mid`),
-  CONSTRAINT `submenu_ibfk_1` FOREIGN KEY (`mid`) REFERENCES `mainmenu` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=utf8;
+-- ----------------------------
+-- Records of submenu
+-- ----------------------------
+INSERT INTO `submenu` VALUES (101, '用户列表', '/user', 100);
+INSERT INTO `submenu` VALUES (102, '修改权限', '/rights', 100);
+INSERT INTO `submenu` VALUES (103, '會員列表', '/uservip', 100);
+INSERT INTO `submenu` VALUES (201, '运动科普', '/Introduction', 200);
+INSERT INTO `submenu` VALUES (202, '卡路里', '/calories', 200);
+INSERT INTO `submenu` VALUES (203, '运动類別', '/sport', 200);
+INSERT INTO `submenu` VALUES (301, '商品模块', '/goods', 300);
+INSERT INTO `submenu` VALUES (302, '营养配餐', '/food', 300);
 
-/*Data for the table `submenu` */
-
-insert  into `submenu`(`id`,`title`,`path`,`mid`) values 
-(101,'用户列表','/user',100),
-(102,'修改权限','/rights',100),
-(103,' 运动模块','/sport',100),
-(104,'商品模块','/goods',100),
-(201,'运动科普','/Introduction',200),
-(202,'卡路里','/calories',200),
-(203,'营养配餐','/food',200);
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+SET FOREIGN_KEY_CHECKS = 1;
